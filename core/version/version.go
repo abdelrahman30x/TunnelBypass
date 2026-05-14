0a3c014c10c6f0309fa2ee56c38026bdd48ab59f
Abdelrahman Mostafa <dev.abdelrahman.30@gmail.com>
2026-05-14 04:03:59 -0700

    chore(release): bump version to 1.3.5

--- core/version/version.go
+++ core/version/version.go
@@ -3,5 +3,5 @@ package version
 // Version is the application version string.
 // It is usually overridden at build time via ldflags:
 //
-//	go build -ldflags "-X 'tunnelbypass/core/version.Version=v1.3.4'"
-var Version = "v1.3.4"
+//	go build -ldflags "-X 'tunnelbypass/core/version.Version=v1.3.5'"
+var Version = "v1.3.5"
